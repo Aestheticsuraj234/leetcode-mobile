f825c8afc9106cb63df592a9432bf08b7e270a90
Aestheticsuraj234 <jhapriya458@gmail.com>
2026-07-05 15:54:52 +0530

    Update environment variables and add new dependencies for improved functionality. Added SUPABASE_SERVICE_ROLE_KEY and CODEBOX_API_TOKEN to .env, and included expo-server in package.json and package-lock.json.

---
+++ src/lib/run-task.ts
@@ -0,0 +1,15 @@
+import { runTask } from 'expo-server'
+
+
+export function runTaskAsync<T>(fn: () => Promise<T>) {
+    return new Promise<T>((resolve, reject) => {
+      runTask(async () => {
+        try {
+          resolve(await fn())
+        } catch (error) {
+          reject(error)
+        }
+      })
+    })
+  }
+  
