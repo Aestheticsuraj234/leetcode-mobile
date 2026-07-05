51d59aebc9fff02b0cc49d01da7aa9f1cb35fec4
Aestheticsuraj234 <jhapriya458@gmail.com>
2026-07-05 14:33:42 +0530

    Remove unused index.tsx file to streamline app structure and eliminate unnecessary components.

---
+++ src/app/(tabs)/problems/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+const ProblemIndex = () => {
+  return (
+    <View>
+      <Text>ProblemIndex</Text>
+    </View>
+  )
+}
+
+export default ProblemIndex
+
+const styles = StyleSheet.create({})
